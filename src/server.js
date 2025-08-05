13cbf77c986d31984dc64e8f6d99386e2901e2aa
wlstn <wlstn159159@naver.com>
2025-08-05 22:32:04 +0900

    refactor: app.listen에서 createServer 후 httpServer.listen 방식으로 변경

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
+// 외부 모듈
+import { createServer } from "http";
+
+// 내부 모듈
 import app from "./app.js";
 
-app.listen(5001, () => {
+const httpServer = createServer(app);
+
+httpServer.listen(5001, () => {
     console.log("Server is running"); //서버가 실행 중인 포트를 출력합니다.
 });
