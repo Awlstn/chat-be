6e0ebf7429e22e74a98d27d8bbe79f7c97386894
wlstn <wlstn159159@naver.com>
2025-08-05 22:36:16 +0900

    chore: .env 환경 변수로 서버 포트 설정 적용

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); // .env 파일의 환경 변수를 로드합니다.
 
 const app = express();
 
--- src/server.js
+++ src/server.js
@@ -6,6 +6,6 @@ import app from "./app.js";
 
 const httpServer = createServer(app);
 
-httpServer.listen(5001, () => {
-    console.log("Server is running"); //서버가 실행 중인 포트를 출력합니다.
+httpServer.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`); // 서버가 실행 중인 포트를 출력합니다.
 });
