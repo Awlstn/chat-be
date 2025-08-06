dfeb29edf89e6a288f691c873dd529c509edbad5
wlstn <wlstn159159@naver.com>
2025-08-06 16:30:41 +0900

    feat: 로그인 컨트롤러 로직 구현

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken";
 
 import User from "../models/User.js";
 
-// 회원가입 signup - POST
+// 회원가입 signupUser - POST
 const singupUser = asyncHandler(async (req, res) => {
     const { userId, password } = req.body;
 
@@ -21,4 +21,28 @@ const singupUser = asyncHandler(async (req, res) => {
     res.status(201).json({ message: "회원가입이 완료되었습니다." });
 });
 
-export { singupUser };
+// 로그인 loginUser - POST
+const loginUser = asyncHandler(async (req, res) => {
+    const { userId, password } = req.body;
+
+    const user = await User.findOne({ userId: userId });
+
+    if (!user) {
+        return res.status(404).json({ message: "일치하는 사용자가 없습니다." });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+        return res
+            .status(401)
+            .json({ message: "비밀번호가 일치하지 않습니다." });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    res.cookie("token", token, {
+        httpOnly: true,
+    });
+    res.status(200).json({ message: "로그인 성공" });
+});
+
+export { singupUser, loginUser };
--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -1,8 +1,9 @@
 import express from "express";
 
-import { singupUser } from "../controllers/authController.js";
+import { singupUser, loginUser } from "../controllers/authController.js";
 
 const router = express.Router();
 router.route("/singup").post(singupUser);
+router.route("/login").post(loginUser);
 
 export default router;
