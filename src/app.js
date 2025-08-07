a02dcde893446fa282384a9259bdf7b029d91274
wlstn <wlstn159159@naver.com>
2025-08-07 22:10:52 +0900

    feat: CORS 미들웨어 설정하여 클라이언트 도메인 허용

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRouter from "../src/routers/authRouter.js";
 
@@ -7,6 +8,11 @@ dotenv.config(); // .env 파일의 환경 변수를 로드합니다.
 
 const app = express();
 app.use(express.json());
+app.use(
+    cors(
+        { origin: "http://localhost:5173" }, // 클라이언트의 도메인을 지정합니다.
+    ),
+);
 app.use("/chat", authRouter);
 
 export default app;
