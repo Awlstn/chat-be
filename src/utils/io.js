883567a21191db0cb6e926de4d12da92441ca140
wlstn <wlstn159159@naver.com>
2025-08-08 19:51:35 +0900

    feat: socket 통신으로 메세지 받기

--- src/utils/io.js
+++ src/utils/io.js
@@ -1,6 +1,10 @@
 const registerSocketEvents = (io) => {
     io.on("connection", async (socket) => {
         console.log("A user connected:", socket.id);
+
+        socket.on("message", async (message) => {
+            console.log("넘어온 메세지 : ", message);
+        });
     });
 };
 
