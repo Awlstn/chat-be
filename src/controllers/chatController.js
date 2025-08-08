46b3f0c822ed928c54710d7b46af05ded72b237f
wlstn <wlstn159159@naver.com>
2025-08-08 23:26:12 +0900

    fix: req, res 인자 값 위치 변경

--- src/controllers/chatController.js
+++ src/controllers/chatController.js
@@ -1,15 +1,17 @@
 import asyncHandler from "express-async-handler";
-import Room from "../models/Room";
+import Room from "../models/Room.js";
 
 // 방 만드는 함수
-const createRoom = asyncHandler(async (res, req) => {
+const createRoom = asyncHandler(async (req, res) => {
     const { name, type, id } = req.body;
     const room = await Room.create({
         name,
         type,
         participants: [id],
     });
-    return res.status(201).json({ message: "채팅방이 만들어졌습니다." });
+    return res
+        .status(201)
+        .json({ message: "채팅방이 만들어졌습니다.", roomId: room._id });
 });
 
 export { createRoom };
