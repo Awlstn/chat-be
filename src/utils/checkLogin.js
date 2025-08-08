21fca21b8dbac167fabd709f890fdf408751e104
wlstn <wlstn159159@naver.com>
2025-08-08 21:28:57 +0900

    feat: 로그인 체크 미들웨어 함수 추가

---
+++ src/utils/checkLogin.js
@@ -0,0 +1,15 @@
+const checkLogin = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({ message: "로그인이 필요합니다." }); // 401 Unauthorized: 인증 필요
+    }
+    jwt.verify(token, jwtSecretKey, (err, decoded) => {
+        if (err) {
+            return res
+                .status(403)
+                .json({ message: "유효하지 않은 토큰입니다." }); // 403 Forbidden: 권한 없음
+        }
+        req.userId = decoded.userId;
+        next();
+    });
+};
