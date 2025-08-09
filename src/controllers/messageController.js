23d501f7c6d49274c2a3dac7ce0c7a67e51c3260
wlstn <wlstn159159@naver.com>
2025-08-09 18:34:58 +0900

    feat: 특정 채팅방 메세지 기록 조회 함수 추가

---
+++ src/controllers/messageController.js
@@ -0,0 +1,13 @@
+import asyncHandler from "express-async-handler";
+import Message from "../models/Message.js";
+
+// 특정 채팅방 메세지 기록 조회
+const getRoomMessages = asyncHandler(async (req, res) => {
+    const { roomId } = req.query;
+    const roomMessages = await Message.find({
+        roomId,
+    });
+    res.status(200).json({ roomMessages: roomMessages });
+});
+
+export { getRoomMessages };
