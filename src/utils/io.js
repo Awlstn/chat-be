b5dde82587a09a136b9dbaf9b0eb41f876d7d549
wlstn <wlstn159159@naver.com>
2025-08-09 17:55:27 +0900

    feat: socket.io joinRoom 이벤트 구현

--- src/utils/io.js
+++ src/utils/io.js
@@ -5,6 +5,11 @@ const registerSocketEvents = (io) => {
         socket.on("message", async (message) => {
             console.log("넘어온 메세지 : ", message);
         });
+
+        //
+        socket.on("joinRoom", (roomId) => {
+            socket.join(roomId); // socket.io가 내부적으로 이 socket을 roomId 그룹에 넣음
+        });
     });
 };
 
