786560714e0fa351dae75a56176c0584fc9ac94e
wlstn <wlstn159159@naver.com>
2025-08-21 20:49:47 +0900

    feat: 소켓 초기화 모듈 추가 및 server.js 변경

--- src/server.js
+++ src/server.js
@@ -4,20 +4,15 @@ import { createServer } from "http";
 // 내부 모듈
 import app from "./app.js";
 import dbConnect from "./config/dbConnect.js";
-import { Server } from "socket.io";
+import { initSocket } from "./utils/socketServer.js";
 import registerSocketEvents from "./utils/io.js";
 
 dbConnect(); // DB 연결
 
 const httpServer = createServer(app);
 
-const socket = new Server(httpServer, {
-    cors: {
-        origin: process.env.CLIENT_URL, // 클라이언트의 도메인을 지정합니다.
-    },
-});
-
-registerSocketEvents(socket);
+const io = initSocket(httpServer);
+registerSocketEvents(io);
 
 httpServer.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`); // 서버가 실행 중인 포트를 출력합니다.
