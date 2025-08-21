6717415471217b81c7db4e040a78c6a51ca103c3
wlstn <wlstn159159@naver.com>
2025-08-21 18:47:57 +0900

    refactor: jwt.verify()는 공통 로직

--- src/utils/checkLogin.js
+++ src/utils/checkLogin.js
@@ -1,20 +1,18 @@
-import jwt from "jsonwebtoken";
+import { verifyToken } from "./utils/jwt.js";
 
 const checkLogin = (req, res, next) => {
-    const jwtSecretKey = process.env.JWT_SECRET;
     const token = req.cookies.token;
     if (!token) {
-        return res.status(401).json({ message: "로그인이 필요합니다." }); // 401 Unauthorized: 인증 필요
+        return res.status(401).json({ message: "로그인이 필요합니다." });
     }
-    jwt.verify(token, jwtSecretKey, (err, decoded) => {
-        if (err) {
-            return res
-                .status(403)
-                .json({ message: "유효하지 않은 토큰입니다." }); // 403 Forbidden: 권한 없음
-        }
-        req.userId = decoded.userId;
-        next();
-    });
+
+    const decoded = verifyToken(token);
+    if (!decoded) {
+        return res.status(403).json({ message: "유효하지 않은 토큰입니다." });
+    }
+
+    req.userId = decoded.userId;
+    next();
 };
 
 export default checkLogin;
